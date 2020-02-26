ef426364601ecc0ac235bddcc8cd58a0f28a7ef8
Martin Kiefel <mk@nopw.de>
2020-02-26 22:03:12 +0100

    Render density next to players

--- src/Pages/Group.elm
+++ src/Pages/Group.elm
@@ -1,6 +1,7 @@
 module Pages.Group exposing (Model, Msg, init, update, view)
 
 import Api
+import Color
 import Css
 import Forms.AddGame
 import Forms.AddUser
@@ -10,6 +11,23 @@ import Html.Styled.Attributes as Attrs
 import Html.Styled.Events as Events
 import Http
 import Json.Decode as Decode
+import LineChart
+import LineChart.Area
+import LineChart.Axis
+import LineChart.Axis.Intersection
+import LineChart.Axis.Line
+import LineChart.Axis.Range
+import LineChart.Axis.Ticks
+import LineChart.Axis.Title
+import LineChart.Colors
+import LineChart.Container
+import LineChart.Dots
+import LineChart.Events
+import LineChart.Grid
+import LineChart.Interpolation
+import LineChart.Junk
+import LineChart.Legends
+import LineChart.Line
 import Url exposing (Url)
 import Url.Builder
 
@@ -122,11 +140,20 @@ requestLeaderboard secretGroupId toMsg =
         }
 
 
-viewUser : String -> Api.User -> Html.Html msg
-viewUser secretGroupId user =
+type alias Point =
+    { x : Float, y : Float }
+
+
+scoreUser : Api.User -> Float
+scoreUser user =
+    user.player.skill.mu - 2.0 * sqrt user.player.skill.sigma2
+
+
+viewUser : Float -> Float -> String -> Api.User -> Html.Html msg
+viewUser minScore maxScore secretGroupId user =
     let
         score =
-            user.player.skill.mu - 2.0 * sqrt user.player.skill.sigma2
+            scoreUser user
 
         toString precision f =
             let
@@ -134,8 +161,85 @@ viewUser secretGroupId user =
                     10 ^ precision
             in
             (toFloat <| round <| f * scale) / scale
+
+        density x =
+            e
+                ^ (-0.5
+                    * (x - user.player.skill.mu)
+                    ^ 2
+                    / user.player.skill.sigma2
+                  )
+                / sqrt (2 * pi * user.player.skill.sigma2)
+
+        paddedMinScore =
+            minScore - (maxScore - minScore) * 0.4
+
+        paddedMaxScore =
+            maxScore + (maxScore - minScore) * 0.2
+
+        samples =
+            80
+
+        data =
+            List.map (\x -> Point x (density x)) <|
+                List.map
+                    (\i ->
+                        toFloat i
+                            / toFloat samples
+                            * (paddedMaxScore - paddedMinScore)
+                            + paddedMinScore
+                    )
+                <|
+                    List.range 0 samples
+
+        unpaddedAxis length accessor =
+            LineChart.Axis.custom
+                { title = LineChart.Axis.Title.default ""
+                , variable = Just << accessor
+                , pixels = length
+                , range = LineChart.Axis.Range.padded 1 1
+                , axisLine = LineChart.Axis.Line.none
+                , ticks = LineChart.Axis.Ticks.custom (\_ _ -> [])
+                }
+
+        chart =
+            LineChart.viewCustom
+                { x = unpaddedAxis 600 .x
+                , y = unpaddedAxis 50 .y
+                , container =
+                    LineChart.Container.custom
+                        { attributesHtml = []
+                        , attributesSvg = []
+                        , size = LineChart.Container.relative
+                        , margin = LineChart.Container.Margin 0 0 0 0
+                        , id = user.id
+                        }
+                , interpolation = LineChart.Interpolation.default
+                , intersection = LineChart.Axis.Intersection.default
+                , legends = LineChart.Legends.none
+                , events = LineChart.Events.default
+                , junk = LineChart.Junk.default
+                , grid = LineChart.Grid.default
+                , area = LineChart.Area.default
+                , line = LineChart.Line.wider 1
+                , dots = LineChart.Dots.default
+                }
+                [ LineChart.line
+                    (Color.rgb255 96 193 125)
+                    LineChart.Dots.none
+                    "density"
+                    data
+                , LineChart.line
+                    (Color.rgb255 241 70 104)
+                    LineChart.Dots.circle
+                    "score"
+                    [ Point score (density score) ]
+                ]
     in
-    Html.li []
+    Html.li
+        [ Attrs.css
+            [ Css.position Css.relative ]
+        ]
         [ Html.div
             [ Attrs.css
                 [ Css.displayFlex
@@ -176,6 +280,16 @@ viewUser secretGroupId user =
                 ]
                 [ Html.text <| String.fromFloat <| toString 2 score ]
             ]
+        , Html.div
+            [ Attrs.css
+                [ Css.position Css.absolute
+                , Css.bottom Css.zero
+                , Css.left Css.zero
+                , Css.right Css.zero
+                , Css.zIndex (Css.int -5)
+                ]
+            ]
+            [ Html.fromUnstyled chart ]
         ]
 
 
@@ -183,7 +297,21 @@ viewLeaderboard : String -> Maybe Leaderboard -> Html.Html msg
 viewLeaderboard secretGroupId maybeLeaderboard =
     case maybeLeaderboard of
         Just leaderboard ->
-            Html.ol [] (List.map (viewUser secretGroupId) leaderboard.users)
+            let
+                scores =
+                    List.map scoreUser leaderboard.users
+
+                minScore =
+                    Maybe.withDefault 0 (List.minimum scores)
+
+                maxScore =
+                    Maybe.withDefault 100 (List.maximum scores)
+            in
+            Html.ol []
+                (List.map
+                    (viewUser minScore maxScore secretGroupId)
+                    leaderboard.users
+                )
 
         Nothing ->
             Html.p [] [ Html.text "loading..." ]
